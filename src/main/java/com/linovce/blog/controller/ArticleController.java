8a10cefcbad12d001585d5d0e5c2cf18746d1386
linovce <1076150551@qq.com>
2021-04-15 16:53:40 +0800

    修改文章管理功能

--- src/main/java/com/linovce/blog/controller/ArticleController.java
+++ src/main/java/com/linovce/blog/controller/ArticleController.java
@@ -8,6 +8,7 @@ import com.linovce.blog.exception.BusinessException;
 import com.linovce.blog.service.ArticleService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import oracle.jrockit.jfr.StringConstantPool;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,10 +28,10 @@ import java.util.List;
 @Controller
 @Api(tags = "文章管理")
 public class ArticleController {
-    @Autowired
-    ArticleService articleService;
     private final static Logger logger = LoggerFactory.getLogger(ArticleController.class);
 
+    @Autowired
+    ArticleService articleService;
 
     @ApiOperation(value = "插入文章", notes="插入文章")
     @RequestMapping(value = "/insertArticle",method = RequestMethod.POST)
@@ -45,7 +46,7 @@ public class ArticleController {
     @ApiOperation(value = "查询文章", notes="通过文章编号查询文章")
     @RequestMapping(value ="/selectArticle",method = RequestMethod.GET)
     @ResponseBody
-    public Article selectArticle(@RequestBody int articleId) throws Exception {
+    public Article selectArticle(@RequestParam(name = "articleId") int articleId) throws Exception {
         logger.info("开始通过文章编号查询文章,文章编号为：{}",articleId);
         Article result =  articleService.select(articleId);
         return result;
